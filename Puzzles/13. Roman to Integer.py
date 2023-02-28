1af3330ea514128b837d2d30bf3159f7184ab916
Peterson, Teran Eric <tpeterson60@gatech.edu>
2023-02-27 22:36:58 -0700

    an apple a day

---
+++ Puzzles/13. Roman to Integer.py
@@ -0,0 +1,35 @@
+# WORKING
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        x = {
+            "I": 1,
+            "IV": 4,
+            "V": 5,
+            "IX": 9,
+            "X": 10,
+            "XL": 40,
+            "L": 50,
+            "XC": 90,
+            "C": 100,
+            "CD": 400,
+            "D": 500,
+            "CM": 900,
+            "M": 1000,
+        }
+        n = 0
+        i = 0
+        while i < len(s):
+            try:
+                n += x[s[i:i+2]]
+                i += 2
+            except:
+                n += x[s[i]]
+                i += 1
+        return n
+
+
+s = Solution()
+print(s.romanToInt("III"))
+print(s.romanToInt("LVIII"))
+print(s.romanToInt("MCMXCIV"))
