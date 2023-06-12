2afb766ceb653189f3e03020ee60cae95bdfc311
Peterson, Teran Eric <tpeterson60@gatech.edu>
2023-06-12 16:24:53 -0600

    study time

---
+++ Puzzles/189. Rotate Array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rotate(self, nums, k):
+        l = len(nums)
+        count = 0
+        start = 0
+        while count < l:
+            i = start
+            temp = nums[i]
+            while True:
+                count += 1
+                j = (i + k) % l
+                swap = nums[j]
+                nums[j] = temp
+                temp = swap
+                if j == start:
+                    break
+                else:
+                    i = j
+            start += 1
+
+s = Solution()
+nums = [-1,-100,3,99]
+s.rotate(nums, 3)
+print(nums)
---
+++ Puzzles/7. Reverse Integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        max_int = 2**31 - 1
+        min_int = -2**31
+        
+        n = 0
+        while (x > 0):
+            print(n*10 + x%10)
+            n = n*10 + x%10
+            x = x/n
+        
+        if n > max_int or n < min_int:
+            return 0
+        return n
+
+s = Solution()
+print(s.reverse(123))
+print(s.reverse(-123))
+print(s.reverse(120))
+print(s.reverse(1534236469))
