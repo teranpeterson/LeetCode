8109280e3e564fb56f0c7e386d9c3b2aede8c6c2
Teran ⠠⠵ <me@teranpeterson.com>
2026-04-09 23:30:16 -0400

    linked lists

---
+++ Study Plans/Top Interview 150/Linked List Cycle.py
@@ -0,0 +1,50 @@
+# SOLVED
+
+# Given head, the head of a linked list, determine if the linked list
+# has a cycle in it.
+
+# There is a cycle in a linked list if there is some node in the list
+# that can be reached again by continuously following the next pointer.
+# Internally, pos is used to denote the index of the node that tail's
+# next pointer is connected to. Note that pos is not passed as a parameter.
+
+# Return true if there is a cycle in the linked list. Otherwise, return false.
+
+# Option to use fast and slow pointers. Guaranteed that they will eventually meet if there is a cycle.
+# Otherwise, fast gets to the end fast.
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+from typing import Optional
+
+class Solution:
+    def __init__(self):
+        self.visited = []
+
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        n = head
+        while n:
+            if n in self.visited:
+                return True
+            self.visited.append(n)
+            n = n.next
+
+        return False
+
+s = Solution()
+
+a = ListNode(3)
+b = ListNode(2)
+c = ListNode(0)
+d = ListNode(-4)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = b
+
+print(s.hasCycle(a)) # true
