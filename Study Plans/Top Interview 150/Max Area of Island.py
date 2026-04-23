9c17981fb92b70df76062852c84c35c0cc3eb9d7
Teran ⠠⠵ <me@teranpeterson.com>
2026-04-22 23:09:04 -0400

    more

--- Study Plans/Top Interview 150/Max Area of Island.py
+++ Study Plans/Top Interview 150/Max Area of Island.py
@@ -1,12 +1,29 @@
+# SOLVED
 
 from typing import List
 
 def maxAreaOfIsland(grid: List[List[int]]) -> int:
-    pass
+    def dfs(i: int, j: int) -> int:
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+            return 0
+        val = grid[i][j]
+        if val != 1:
+            return 0
+        grid[i][j] = 2
+        return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
+    
+    res = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            val = grid[i][j]
+            if val != 1:
+                continue
+            res = max(res, dfs(i, j))
+    return res
+          
 
 print(maxAreaOfIsland([
-  [0,1,1,0,1],
-  [1,0,1,0,1],
-  [0,1,1,0,1],
-  [0,1,0,0,1]
-])) # 6
+[0,1,1,0,1],
+[1,0,1,0,1],
+[0,1,1,0,1],
+[0,1,0,0,1]])) # 6
